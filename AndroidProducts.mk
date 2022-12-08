ea5a3c13502be415eb50f98ff422ca9fe073aab6
karthik990 <knag88@gmail.com>
2022-12-08 14:38:14 +0000

    phone1: Adapt to derp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_phone1.mk
+    $(LOCAL_DIR)/derp_phone1.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_phone1-eng \
-    aosp_phone1-user \
-    aosp_phone1-userdebug
+    derp_phone1-eng \
+    derp_phone1-user \
+    derp_phone1-userdebug
--- derp_phone1.mk
+++ derp_phone1.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/nothing/phone1/device.mk)
 
-# Inherit from the Nameless AOSP configuration.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit from the Derpfest OS configuration.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Bootanimation resolution.
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -22,7 +22,7 @@ PRODUCT_BRAND := Nothing
 PRODUCT_DEVICE := phone1
 PRODUCT_MANUFACTURER := Nothing
 PRODUCT_MODEL := A063
-PRODUCT_NAME := aosp_phone1
+PRODUCT_NAME := derp_phone1
 PRODUCT_SYSTEM_DEVICE := Spacewar
 PRODUCT_SYSTEM_NAME := Spacewar
 
